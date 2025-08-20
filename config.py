b0840a95da946bec9cb10e7c65dd38f252d83e4c
sraghav42 <sraghav42@gmail.com>
2025-08-19 21:28:12 -0400

    Update RSS feed list in config.py

--- config.py
+++ config.py
@@ -5,12 +5,9 @@ load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 RSS_FEEDS = [
-    # "https://www.theverge.com/rss/index.xml",
-    # "https://www.technologyreview.com/feed/",
-    # "https://spectrum.ieee.org/rss/fulltext",
     "https://feeds.bbci.co.uk/news/technology/rss.xml",
-    "https://rss.nytimes.com/services/xml/rss/nyt/Technology.xml",
-    "https://feeds.arstechnica.com/arstechnica/technology",
-    "https://www.zdnet.com/news/rss.xml",
-    "https://www.cnet.com/rss/news/"
+    "https://www.cnet.com/rss/news/",
+    "https://www.engadget.com/rss.xml",
+    "https://www.techradar.com/rss",
+    "https://www.npr.org/rss/rss.php?id=1019"
 ]
